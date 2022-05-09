6fdb2c3694036f720e624285f42481aab6bee30f
ersin.demircan <e.mertdemircan@gmail.com>
2022-05-09 22:36:35 +0300

    Get method chenged

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -24,16 +24,13 @@ namespace gitproject.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public ActionResult<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok(new WeatherForecast{
+                Date = new DateTime(2022,10,25),
+                TemperatureC = 10,
+                Summary = "25 ekim 2022 hava durumu"
+            });
         }
     }
 }
