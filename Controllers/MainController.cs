b1153965c05acdf137127fd03f422c233f007c97
ersin.demircan <e.mertdemircan@gmail.com>
2022-05-11 15:24:23 +0300

    Main Controller added

---
+++ Controllers/MainController.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gitproject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController: ControllerBase{
+        [HttpGet]
+        public IActionResult Get(){
+            return Ok("ok");
+        }
+    }
+}
--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace gitproject.Controllers
 {
@@ -11,20 +7,8 @@ namespace gitproject.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
-        public ActionResult<WeatherForecast> Get()
+        public IActionResult Get()
         {
             return Ok(new WeatherForecast{
                 Date = new DateTime(2022,10,25),
