1da42628fdbf80f0a959dbd614c7d47c444a8132
ersin.demircan <e.mertdemircan@gmail.com>
2022-05-14 16:31:45 +0300

    Bet Controller added

---
+++ Controllers/BetController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gitproject.Controllers
+{
+    [Route("[controller]")]
+    public class BetController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Name = "Manchester - Real Madrid",
+                Date = new DateTime(2022, 6, 20)
+            });
+        }
+    }
+}
