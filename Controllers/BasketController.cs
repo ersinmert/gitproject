2e46283a283ad9730cbb446c4c133b3efaada7de
ersinmert <ersinmert_demircan@hotmail.com>
2022-05-17 14:12:37 +0300

    Basket Controller added

---
+++ Controllers/BasketController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gitproject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BasketController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Baseket get");
+        }
+    }
+}
